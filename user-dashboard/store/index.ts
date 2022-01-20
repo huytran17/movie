01695d10411dab6ee6ddcbf56092c2bf6504a372
hahaha1618 <alpha.lloyd1368@gmail.com>
2022-01-20 22:58:54 +0700

    test for vuex

---
+++ user-dashboard/store/index.ts
@@ -0,0 +1,9 @@
+import { ActionTree, MutationTree } from 'vuex'
+
+export const state = () => ({})
+
+export type RootState = ReturnType<typeof state>;
+
+export const actions: ActionTree<RootState, RootState> = {}
+
+export const mutations: MutationTree<RootState> = {}
