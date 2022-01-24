55d35f4733fdbed8a30be8ad45a1c96f6947c29a
hahaha1618 <alpha.lloyd1368@gmail.com>
2022-01-24 22:51:03 +0700

    left side box

---
+++ user-dashboard/components/LeftSideBox.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="ml-auto side-box-wrapper mt-3">
+    <div>
+      <BirthdayBox />
+    </div>
+    <div>
+      <ContactBox />
+    </div>
+  </div>
+</template>
+
+<script>
+import BirthdayBox from "@/components/BirthdayBox";
+import ContactBox from "@/components/ContactBox";
+
+export default {
+  name: "LeftSideBox",
+  components: {
+    BirthdayBox,
+    ContactBox,
+  },
+};
+</script>
+
+<style scoped>
+.side-box-wrapper {
+  width: 300px;
+}
+</style>
