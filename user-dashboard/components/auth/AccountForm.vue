f2e0a0ea5b97f0bef68cc31ea1a552a74696ea16
huytran17 <alpha.lloyd1368@gmail.com>
2022-02-19 12:55:54 +0700

    account form

---
+++ user-dashboard/components/auth/AccountForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <form>
+    <v-text-field
+      v-model="email"
+      label="Name"
+      required
+      :rules="emailRules"
+    ></v-text-field>
+    <v-text-field
+      v-model="password"
+      label="E-mail"
+      required
+      :rules="passwordRules"
+    ></v-text-field>
+    <v-btn class="mr-4" @click="login"> {{ $t("Login") }} </v-btn>
+  </form>
+</template>
+
+<script>
+import authMixin from "@/mixins/auth";
+export default {
+  name: "AccountForm",
+  mixins: [authMixin],
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+};
+</script>
