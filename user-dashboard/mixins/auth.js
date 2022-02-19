2916c7f9173540475f9e3828db6b54f177690b97
huytran17 <alpha.lloyd1368@gmail.com>
2022-02-19 12:55:48 +0700

    login rules

--- user-dashboard/mixins/auth.js
+++ user-dashboard/mixins/auth.js
@@ -1,6 +1,18 @@
 import { mapGetters, mapActions, mapMutations } from "vuex";
 
 export default {
+  data() {
+    return {
+      emailRules: [
+        (v) => !!v || "E-mail is required",
+        (v) => /.+@.+\..+/.test(v) || "E-mail must be valid.",
+      ],
+      passwordRules: [
+        (v) => !!v || "Password is required",
+        (v) => (v && v.length > 7) || "Password must be min 8 characters",
+      ],
+    };
+  },
   computed: {
     ...mapGetters({
       user: "auth/user",
