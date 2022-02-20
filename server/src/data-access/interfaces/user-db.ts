657b37d48f3386c00c01b412c4f31a28601a3b34
huytran17 <alpha.lloyd1368@gmail.com>
2022-02-20 14:54:50 +0700

    user data access

--- server/src/data-access/interfaces/user-db.ts
+++ server/src/data-access/interfaces/user-db.ts
@@ -1,11 +1,16 @@
 import User from "../../entities/user";
 import IUser from "../../interfaces/user";
+import { ClientSession } from "mongoose";
 
 export default interface IUserDb {
   findAll: () => Promise<IUser[] | null>;
   findOne: () => Promise<User | null>;
-  insert: (payload: Partial<IUser>) => Promise<User | null>;
   findById: ({ id }: { id: string }) => Promise<User | null>;
+  findByEmail: ({ email }: { email: string }) => Promise<User | null>;
+  insert: (
+    payload: Partial<IUser>,
+    options?: { session?: ClientSession }
+  ) => Promise<User | null>;
 }
 
 export interface PaginatedUserResult {
--- server/src/data-access/make-user-db.ts
+++ server/src/data-access/make-user-db.ts
@@ -15,10 +15,18 @@ export default function makeUserDb({
   moment: any;
 }): IUserDb {
   return new (class MongooseUserDb implements IUserDb {
+    /**
+     *
+     * @returns find all
+     */
     async findAll(): Promise<User[] | null> {
       return null;
     }
-
+    /**
+     * insert new record
+     * @param payload
+     * @returns
+     */
     async insert(payload: Partial<IUser>): Promise<User | null> {
       const result = await userDbModel.create([payload]);
 
@@ -30,7 +38,11 @@ export default function makeUserDb({
       }
       return null;
     }
-
+    /**
+     * find by id
+     * @param param0
+     * @returns
+     */
     async findById({ id }: { id: string }): Promise<User | null> {
       const existing = await userDbModel
         .findOne({ _id: id })
@@ -43,8 +55,29 @@ export default function makeUserDb({
       return null;
     }
 
+    /**
+     * find one record
+     * @returns
+     */
     async findOne(): Promise<User | null> {
       const existing = await userDbModel.findOne().lean({ virtuals: true });
+
+      if (existing) {
+        return new User(existing);
+      }
+
+      return null;
+    }
+
+    /**
+     * find by email
+     * @param param0
+     * @returns
+     */
+    async findByEmail({ email }: { email: string }): Promise<User | null> {
+      const existing = await userDbModel
+        .findOne({ email })
+        .lean({ virtuals: true });
 
       if (existing) {
         return new User(existing);
