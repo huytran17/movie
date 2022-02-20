bb29cf8d8bf2a5f2276e6550d29e7e3c0f1255d2
huytran17 <alpha.lloyd1368@gmail.com>
2022-02-20 12:34:36 +0700

    login use case

--- server/src/routes/api/auth.ts
+++ server/src/routes/api/auth.ts
@@ -4,14 +4,14 @@ import authenticateJWT from "../../middlewares/authenticateJWT";
 import makeValidator from "../../middlewares/validator-middleware";
 import makeExpressCallback from "../../express-callback";
 import { loginRules } from "../../controllers/auth/validators";
-import { loginController } from "../../controllers/auth";
+// import { loginController } from "../../controllers/auth";
 
 const authRouter = express.Router();
 
 authRouter.post(
   "/login",
-  makeValidator(loginRules),
-  makeExpressCallback(loginController)
+  makeValidator(loginRules)
+  // makeExpressCallback(loginController)
 );
 
 export default authRouter;
