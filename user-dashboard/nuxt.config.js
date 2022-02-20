ac738827ed5eec246212af8256a100b2be617b33
huytran17 <alpha.lloyd1368@gmail.com>
2022-02-20 20:50:44 +0700

    new pkg

--- user-dashboard/nuxt.config.js
+++ user-dashboard/nuxt.config.js
@@ -27,7 +27,7 @@ export default {
   css: ["~/assets/global/css/style"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [{ src: "~/plugins/vue-toastification", ssr: false }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
