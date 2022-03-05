b1178e9efc455975f018cd7026c6c0142c18adfc
huytran17 <alpha.lloyd1368@gmail.com>
2022-03-05 21:16:24 +0700

    user route

--- server/src/routes/api/user.ts
+++ server/src/routes/api/user.ts
@@ -2,10 +2,17 @@ import express from "express";
 import makeValidator from "../../middlewares/validator-middleware";
 import makeExpressCallback from "../../express-callback";
 
+import { getUserRules } from "../../controllers/user/validators";
+import { getUserController } from "../../controllers/user";
+
 const userRouter = express.Router();
 
-userRouter.get("/", (req, res) => {
-  res.json({ message: "ok" });
-});
+userRouter.get(
+  "/:user_id",
+  makeValidator(getUserRules),
+  makeExpressCallback(getUserController),
+);
+
+
 
 export default userRouter;
