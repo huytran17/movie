9a32f909a511fa741ea6a9212762d72c5f4522bb
huytran17 <alpha.lloyd1368@gmail.com>
2022-03-06 17:17:09 +0700

    update

--- server/src/schemas/user.ts
+++ server/src/schemas/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import _ from "lodash";
 
 const Schema = mongoose.Schema;
 
@@ -15,6 +16,24 @@ const userSchema = new Schema({
   updated_at: { type: Date, default: Date.now },
   deleted_at: { type: Date, default: null },
   email_verified_at: { type: Date, default: null },
+});
+
+userSchema.virtual("full_name").get(() => {
+  const user = this as any;
+  const first_name = _.get(user, "first_name", "");
+  const last_name = _.get(user, "last_name", "");
+  const fullname = `${first_name} ${last_name}`;
+
+  return fullname;
+});
+
+userSchema.virtual("alias_name").get(() => {
+  const user = this as any;
+  const { full_name } = user;
+  const matches = full_name.match(/\b(\w)/g); // ['J','S','O','N']
+  const acronym = matches.join(""); // JSON
+
+  return acronym;
 });
 
 export default userSchema;
