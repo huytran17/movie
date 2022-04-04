9e60fe72fce6414bbcb941bea87256a986b1f5f5
huytran17 <alpha.lloyd1368@gmail.com>
2022-04-04 14:17:48 +0700

    film category

--- server/src/interfaces/film.ts
+++ server/src/interfaces/film.ts
@@ -6,8 +6,19 @@ export default interface IAdmin {
   thumnail_url: string;
   slug: string;
   views: number;
+  category: CategoryType;
   manufactured_at: Date;
   created_at: Date;
   updated_at: Date;
   deleted_at: Date;
+}
+
+export enum CategoryType {
+  Comedy = "comedy",
+  Horror = "horror",
+  Documentary = "documentary",
+  Family = "family",
+  Kid = "kid",
+  Vietnam = "vietnam",
+  USUK = "usuk",
 }
