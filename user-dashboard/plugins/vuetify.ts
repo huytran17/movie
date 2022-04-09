7167bdf1401e7687c5ef59e9e1b895e1cba8eec9
huytran17 <alpha.lloyd1368@gmail.com>
2022-04-10 02:19:08 +0700

    tiptap plugin

--- user-dashboard/plugins/vuetify.ts
+++ user-dashboard/plugins/vuetify.ts
@@ -1,6 +1,6 @@
 ///<reference path="./vuetify.d.ts" />
 
-import "@mdi/font/css/materialdesignicons.css"; // Ensure you are using css-loader
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
 
