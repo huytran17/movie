d482652cf2959d44ea65f38a98623ca0c3cee547
huytran17 <alpha.lloyd1368@gmail.com>
2022-05-12 20:54:24 +0700

    no content

---
+++ admin-dashboard/components/BaseNoContent.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-container>
+    <div class="text-h6">
+      <span class="app-title">
+        <span v-html="$t('No data found..')"></span>
+      </span>
+    </div>
+    <div class="text-body-2">
+      <v-btn color="primary" @click="$router.push(localePath(''))" depressed>
+        <span class="app-body">
+          <span v-html="$t('Back to home')"></span>
+        </span>
+      </v-btn>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ user-dashboard/components/BaseNoContent.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container class="d-flex justify-center pt-10">
+    <div class="d-flex flex-column justify-center">
+      <div class="text-h6">
+        <span class="app-title">
+          <span v-html="$t('No data found..')"></span>
+        </span>
+      </div>
+      <div>
+        <v-img :src="no_data_image" max-width="300px"></v-img>
+      </div>
+      <div class="text-body-2 d-flex justify-center">
+        <v-btn color="primary" @click="$router.push(localePath(''))" depressed>
+          <span class="app-body">
+            <span v-html="$t('Back to home')"></span>
+          </span>
+        </v-btn>
+      </div>
+    </div>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "BaseNoContent",
+  data() {
+    return {
+      no_data_image: require("@/assets/images/empty_item.svg"),
+    };
+  },
+};
+</script>
+
+<style></style>
