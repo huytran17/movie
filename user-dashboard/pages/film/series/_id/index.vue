5ee1099932ea99eb28a421acea775829599257f1
huytran17 <alpha.lloyd1368@gmail.com>
2022-05-21 11:32:27 +0700

    series no content

--- user-dashboard/pages/film/series/_id/index.vue
+++ user-dashboard/pages/film/series/_id/index.vue
@@ -1,16 +1,18 @@
 <template>
-  <BaseFilmsList2 :films_list="films" :show_title="true" />
+  <BaseNoContent v-if="films.length <= 0" />
+  <BaseFilmsList2 v-else :films_list="films" :show_title="true" />
 </template>
 
 <script>
 import BaseFilmsList2 from "@/components/film/BaseFilmsList2";
 import seriesMixins from "@/mixins/series";
 import filmMixins from "@/mixins/film";
+import BaseNoContent from "@/components/BaseNoContent";
 
 export default {
   name: "Films",
   mixins: [seriesMixins, filmMixins],
-  components: { BaseFilmsList2 },
+  components: { BaseFilmsList2, BaseNoContent },
   async fetch() {
     try {
       const series_id = this.$route.params.id;
